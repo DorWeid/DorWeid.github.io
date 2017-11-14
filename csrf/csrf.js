247f08144407570f6a91f3d8c622dd43299bea09
DorWeid <dorprozor@gmail.com>
2017-11-14 11:25:47 +0200

    a

--- csrf/csrf.js
+++ csrf/csrf.js
@@ -9,7 +9,10 @@ $(function() {
 		data: 'op=login-main&user=fadingz&passwd=dortheking&api_type=json'
 	}).done(function(response) {
 		console.log('response: ', response);
-		console.log('should redirect');
+		var parsed = JSON.parse(response);
+		console.log('p', parsed);
+		document.cookie = 'reddit_session=' + parsed.json.data.cookie;
+		//45372644463%2C2017-11-14T00%3A50%3A41%2C5b282be83e9ceb2d7776b25b8d8637fd36d7a23a
 	}).fail(function(error) {
 		console.log('failure: ', error);
 	}).always(function() {
